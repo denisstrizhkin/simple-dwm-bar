22bd487298b7563e76cc6319726ff1c50db6ed51
Strizhkin Denis <strdenis02@gmail.com>
2021-08-22 08:48:44 +0300

    date_format change

--- update_date.sh
+++ update_date.sh
@@ -1,9 +1,9 @@
 scripts_dir="/home/denis/.local/bin/DWM_Scripts"
 
 function date_update {
-	date_string="$( date +"%B %-d,%Y|%-I:%M%p") "
-	echo $date_string | tee ${scripts_dir}/savedata/date_string.txt
-	"${scripts_dir}/update_all.sh"
+	date_string="$( date +"%d(%A).%m(%B).%y|%H:%M") "
+	echo $date_string | tee ./savedata/date_string.txt
+	"./update_all.sh"
 }
 
 date_update
