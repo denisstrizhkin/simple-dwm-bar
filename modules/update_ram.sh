b150e4c8c462bd5078eac2ce4d15425b07916da5
Strizhkin Denis <strdenis02@gmail.com>
2021-08-22 10:57:54 +0300

    added update_ram.sh removed update_cuse.sh

---
+++ modules/update_ram.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+update_ram () {
+	# get cpu usage
+	ram_data=$(free -h | grep Mem)
+	ram_all=$(echo $ram_data | awk '{print $2}')
+	ram_used=$(echo $ram_data | awk '{print $3}')
+
+	# format and return
+	printf "RAM:%s/%s" $ram_used $ram_all
+}
+
+update_ram
