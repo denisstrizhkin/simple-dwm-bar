3fe1386d6c822a27978793b385a45742f9ec2572
Denis Strizhkin <strdenis02@gmail.com>
2021-08-23 13:57:14 +0300

    changed update_battert.sh

--- modules/update_battery.sh
+++ modules/update_battery.sh
@@ -1,31 +1,9 @@
-scripts_dir="/home/denis/.local/bin/DWM_Scripts"
+function update_battery {
+	# get battery percentage
+	bat_level=$(cat /sys/class/power_supply/BAT1/capacity)
 
-function battery_update {
-	battery_level="$(cat /sys/class/power_supply/BAT0/capacity)"
-	# 0-10   , 11-30   , 31-50   , 51-75   , 76-100 
-	# Discharging     Charging     Full  
-	#switch battery icon depending on battery level
-	if ((battery_level >= 0 && battery_level <= 10)); then
-		battery_string=""
-	elif ((battery_level >= 11 && battery_level <= 30)); then
-		battery_string=""
-	elif ((battery_level >= 31 && battery_level <= 50)); then
-		battery_string=""
-	elif ((battery_level >= 51 && battery_level <= 75)); then
-		battery_string=""
-    else
-        battery_string=""
-	fi
-
-	b_state="$(cat /sys/class/power_supply/BAT0/status)"
-	if [ "$b_state" = "Charging" ]; then
-		battery_string+=""
-	elif [ "$b_state" = "Full" ]; then
-		battery_string+=""
-	fi
-
-	echo $battery_string | tee ${scripts_dir}/savedata/battery_string.txt
-	"${scripts_dir}/update_all.sh"
+	# format and return
+	printf "B:%2s%%" $bat_level
 }
 
-battery_update
+update_battery
